13f1cb800533f64aac795fdaf0277a18875ee68e
simongrebelatto <79119875+simongrebelatto@users.noreply.github.com>
2021-05-18 14:45:05 -0300

    ComponentsHeaderCreate

---
+++ app-react/src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="main-header-container">
+      <header className="main-header">
+        <h1>Criar Novo Usuário</h1>
+      </header>
+    </div>
+  );
+}
+
+export { Header };
